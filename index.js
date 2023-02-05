834b2aeb51180f606de5876c96f1f5d753c89646
imLines <a.code.room@gmail.com>
2023-02-05 03:24:25 +0100

    Add middleware for AC origin

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const cors = require("cors");
 const port = 8005;
 const bodyParser = require("body-parser");
 
-const corsOptions = {
-  origin: 'https://37.59.180.198',
-}
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://37.59.180.198');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  next();
+});
 
 app.use(bodyParser.json());
 require("dotenv").config();
