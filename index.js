9a465aae42be8affd692a4b74b564a2f79a99eac
imLines <a.code.room@gmail.com>
2023-02-27 16:28:42 +0100

    update cors

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const credentials = {
 
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://wwidev.tech');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Credentials', false);
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  next();
+});
 
 app.use(bodyParser.json());
 require("dotenv").config();
