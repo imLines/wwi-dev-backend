9a9a918333945b0f4e32b576567260fee309856a
imLines <a.code.room@gmail.com>
2023-02-28 15:17:54 +0100

    cors update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const credentials = {
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.wwidev.tech');
+  res.setHeader('Access-Control-Allow-Origin', 'https://wwidev.tech');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', false);
   next();
